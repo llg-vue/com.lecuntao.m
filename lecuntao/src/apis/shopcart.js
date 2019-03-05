f2b8e979769d86bfc25389890796f89726301a1c
979623625@qq.com <979623625@qq.com>
2019-03-05 22:34:07 +0800

    “郭腾飞更改了注册的请求逻辑代码”

--- lecuntao/src/apis/shopcart.js
+++ lecuntao/src/apis/shopcart.js
@@ -1,5 +1,5 @@
 import http from "../utils/http"
 
-export const ShopCart = (params) => http("post", "/api/lct?api_version=2.3.0&platType=2&client=wap&isEncry=0&time=1551618651506", params)
-export const GoodReduce=(params)=>http("post","/api/lct?api_version=2.3.0&platType=2&client=wap&isEncry=0&time=1551626669551",params)
-export const GoodAdd=(params)=>http("post","/api/lct?api_version=2.3.0&platType=2&client=wap&isEncry=0&time=1551689082680",params)
+export const ShopCart = (params) => http("post", "/api/lct?api_version=2.3.0&platType=2&client=wap&isEncry=0&time=1551775332341", params)
+export const GoodReduce = (params) => http("post", "/api/lct?api_version=2.3.0&platType=2&client=wap&isEncry=0&time=1551626669551", params)
+export const GoodAdd = (params) => http("post", "/api/lct?api_version=2.3.0&platType=2&client=wap&isEncry=0&time=1551689082680", params)
